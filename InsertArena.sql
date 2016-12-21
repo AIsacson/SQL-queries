adfb72bf442b075c6514b6b069be514162414917
AIsacson <annaisacson@Annas-MacBook-Pro.local>
2016-12-21 12:05:37 +0100

    update
    Update on the xml

---
+++ InsertArena.sql
@@ -0,0 +1,2 @@
+INSERT INTO arena VALUES('Camp Nou', 'Lund'), ('FNB', 'Köpenhamn'),
+ ('Wembley', 'London'), ('Olympiya', 'Manchester'), ('Friends Arena', 'Stockholm');
---
+++ InsertTurneringsdeltagare.sql
@@ -0,0 +1,7 @@
+INSERT INTO turneringsdeltagare VALUES ('ERF','KronborgCup'),
+('ERF', 'Champions League'), ('ERF', 'FotbollsCupen'),
+('FFG', 'TigerCupen'), ('FFG', 'LassaBollen'),
+('JRT', 'FotbollsCupen'), ('JRT', 'KronborgCup'),
+('JRT', 'Champions League'), ('RTF', 'Champions League'),
+('RTF', 'TigerCupen'), ('SSE', 'LassaBollen'),
+('SSE', 'TigerCupen'), ('SSE', 'KronborgCup');
